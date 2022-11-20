b361918ffbfc3adc236d58f0cd17c216f89f30a9
Igor <dexchanel@gmail.com>
2022-11-19 22:21:01 -0300

    feat(db): db example

---
+++ Server/db/db.py
@@ -0,0 +1,16 @@
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27021/?replicaSet=dbrs&directConnection=true")
+db = client["test"]
+collection = db["test"]
+
+tutorial1 = {
+    "name": "tutorial1",
+    "description": "tutorial1 description",
+    "tags": ["mongodb", "database", "NoSQL"],
+    "likes": 100,
+}
+
+tutorial = db.tutorial
+tutorial.insert_one(tutorial1)
