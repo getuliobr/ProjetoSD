4817900182d394e7c5af2f100a05dfbf72c11423
Igor <dexchanel@gmail.com>
2022-11-22 21:08:45 -0300

    feat(api): start doing stuff with db in client requests

--- Server/db/db.py
+++ Server/db/db.py
@@ -2,15 +2,15 @@ import pymongo
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://localhost:27021/?replicaSet=dbrs&directConnection=true")
-db = client["test"]
-collection = db["test"]
+db = client["utPlace"]
+# collection = db["test"]
 
-tutorial1 = {
-    "name": "tutorial1",
-    "description": "tutorial1 description",
-    "tags": ["mongodb", "database", "NoSQL"],
-    "likes": 100,
-}
+# tutorial1 = {
+#     "name": "tutorial1",
+#     "description": "tutorial1 description",
+#     "tags": ["mongodb", "database", "NoSQL"],
+#     "likes": 100,
+# }
 
-tutorial = db.tutorial
-tutorial.insert_one(tutorial1)
+# tutorial = db.tutorial
+# tutorial.insert_one(tutorial1)
--- Server/main.py
+++ Server/main.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+from pstats import Stats
+import re
+from fastapi import FastAPI, Request
 from numpy import append
+from Server.db.db import db
 
 app = FastAPI()
 
@@ -8,5 +11,23 @@ def get_tiles():
     return {"Hello": "World"}
 
 @app.put("/")
-def put_tile():
+def put_tile(request : Request):
+    print(request.client.host)
+    db.Place.find_one_and_update({"_id": {"x":0, "y":0}},{'$set': {"color": 0x001110}})
+    db.Place.insert_one({
+        "_id": {"x": 4, "y": 4},
+        "color": 0xFF0000
+    })
     return {"Gomu Gomu no mi": "Elephant Gun"}
+
+@app.post('/my-endpoint')
+async def my_endpoint(request: Request):
+    x = 'x-forwarded-for'.encode('utf-8')
+    print(request.headers.raw)
+    for header in request.headers.raw:
+        if header[0] == x:
+            print("Find out the forwarded-for ip address")
+            origin_ip, forward_ip = re.split(', ', header[1].decode('utf-8'))
+            print(f"origin_ip:\t{origin_ip}")
+            print(f"forward_ip:\t{forward_ip}")
+    return {'status': 1, 'message': 'ok'}
