86b8699e7a9a5bf1f6b1e0497a6a502cab6d1c58
Igor <dexchanel@gmail.com>
2022-11-29 21:04:46 -0300

    feat(websocket): added websocket on route /ws

--- Server/main.py
+++ Server/main.py
@@ -1,11 +1,8 @@
-import json
-from pstats import Stats
+import logging
 import time
-from fastapi import FastAPI, HTTPException, Request
+from fastapi import FastAPI, HTTPException, Request, WebSocket, WebSocketDisconnect
 import redis
 
-from starlette.middleware import Middleware
-from starlette.middleware.cors import CORSMiddleware
 from Server.src.db.db import db
 from Server.src.dbHandler import getPlaceFromDb, putTileOnDb, getClientTimeFromDb
 from Server.src.entities.tile import Tile
@@ -13,14 +10,16 @@ from Server.src.entities.user import User
 from Server.src.redisHandler import getPlaceFromRedis
 
 app = FastAPI()
+logger = logging.getLogger()
 
 @app.on_event("startup")
 async def startup_event():
-    app.redisBase = redis.from_url('redis://redis:6379', db=0)
+    app.redisBase = redis.from_url('redis://localhost:6379', db=0)
     app.redisBase.reset()
     app.UserCollection = db.Users
     app.PlaceCollection = db.Place
     app.COOLDOWN = 0
+    app.clients = []
 
 @app.get("/timeStampUser")
 async def timeStampUser(request: Request):
@@ -35,7 +34,7 @@ async def get_tiles():
     place = getPlaceFromRedis()
     return {"place": place}
 
-@app.post("/")
+@app.post("/tile")
 async def put_tile(tile: Tile, request : Request):
     ip = request.client.host
     tile.ip = ip
@@ -46,8 +45,21 @@ async def put_tile(tile: Tile, request : Request):
         tile = putTileOnDb(user, tile, currTime)
         place = getPlaceFromDb()
         app.redisBase.set('place', place)
+        for client in app.clients:
+            await client.send_json(tile)
         return { 'success': True, 'tile': tile }
     except HTTPException as e:
         raise e
     except Exception as e: 
         raise HTTPException(status_code=500, content={'success': False, "message": "Internal not treated server error"})
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    app.clients.append(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+    except WebSocketDisconnect as e:
+        logger.warn(f"Websocket disconnected: {websocket.client.host}")
+        app.clients.remove(websocket)
